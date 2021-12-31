cec4b2cd492d2de8c0532410a5a7db767b1a5f1d
Aaron Westreich <aaronwestreich2001@gmail.com>
2021-12-31 23:05:43 +0200

    Add coloring, resizing pixels and clearing

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const NUM_OF_PIXELS = 16;
 const grid = document.querySelector('#grid');
-
+const clearbtn = document.querySelector('#clear');
+let NUM_OF_PIXELS = 16;
+grid.style.gridTemplateColumns = `repeat(${NUM_OF_PIXELS}, 1fr)`;
+clearbtn.addEventListener('click', clear);
 for (let i = 0; i<NUM_OF_PIXELS*NUM_OF_PIXELS; i++){
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.textContent = i;
+    cell.addEventListener('mouseover', function() {
+        cell.classList.add('cell-hover');
+    });
     grid.appendChild(cell);
+}
+
+function clear() {
+    for (const cell of grid.childNodes) {
+        cell.classList.remove('cell-hover');
+    }
 }
