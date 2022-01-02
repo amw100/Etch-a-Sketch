e5b9354cc51dc6e123fad13883d64453a6854c69
Aaron Westreich <aaronwestreich2001@gmail.com>
2022-01-02 20:27:56 +0200

    Add size slider and final styling

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const grid = document.querySelector('#grid');
+const sizeSlider = document.querySelector('#grid-size');
 const clearbtn = document.querySelector('#clear');
-let NUM_OF_PIXELS = 16;
-grid.style.gridTemplateColumns = `repeat(${NUM_OF_PIXELS}, 1fr)`;
+
 clearbtn.addEventListener('click', clear);
-for (let i = 0; i<NUM_OF_PIXELS*NUM_OF_PIXELS; i++){
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
-    cell.addEventListener('mouseover', function() {
-        cell.classList.add('cell-hover');
-    });
-    grid.appendChild(cell);
+sizeSlider.addEventListener('input', createGrid);
+
+createGrid();
+
+function createGrid(){
+    let NUM_OF_PIXELS = sizeSlider.value;
+    //clear the grid so we don't add on top of it
+    while(grid.firstChild)
+        grid.removeChild(grid.firstChild);
+    grid.style.gridTemplateColumns = `repeat(${NUM_OF_PIXELS}, 1fr)`;
+    for (let i = 0; i<NUM_OF_PIXELS*NUM_OF_PIXELS; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.addEventListener('mouseover', function() {
+            cell.classList.add('cell-hover');
+        });
+        grid.appendChild(cell);
+    }
 }
 
 function clear() {
